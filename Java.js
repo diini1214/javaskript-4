70505987cb78d7ddaa58d9e118e4b8542372d4cd
diini1214 <71126396+diini1214@users.noreply.github.com>
2021-04-15 10:26:05 +0200

    Delete Java.js

--- Java.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('.login_button');
-button.addEventListener('click', login);
-
-function login() {
-    email = document.getElementById("email").value;
-    password = document.getElementById("pwd").value;
-
-
-}
